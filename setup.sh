62272f063778a2501fbd6021c4cabdc2c268f9ce
ivanIRP <ivatar1066@gmail.com>
2026-03-11 18:02:29 -0600

    ajustes

--- setup.sh
+++ setup.sh
@@ -1,93 +1,58 @@
 #!/bin/bash
 
-if [ "$EUID" -ne 0 ]; then
-echo "Ejecuta con sudo"
-exit
-fi
+clear
+echo "CONFIGURADOR FRR"
 
-while true
-do
+echo "1) Configurar R0"
+echo "2) Configurar R1"
+read -p "Selecciona router: " op
 
-echo ""
-echo "1 Configurar Router R0"
-echo "2 Configurar Router R1"
-echo "3 Salir"
+if [ "$op" = "1" ]; then
 
-read -p "Seleccione opcion: " op
+echo "Configurando R0..."
 
-case $op in
-
-1.
-
-cat > /etc/frr/frr.conf <<EOF
-hostname R0
+echo "hostname R0
 service integrated-vtysh-config
-
 ipv6 forwarding
-
 interface enp0s3
 ipv6 address 2001:1::1/64
 ipv6 rip RIP enable
-
 interface enp0s8
 ipv6 address 2001:2::1/64
 ipv6 rip RIP enable
-
 interface enp0s9
 ipv6 address 2001:3::1/64
 ipv6 rip RIP enable
-
 ipv6 router rip RIP
-
-line vty
-EOF
+line vty" > /etc/frr/frr.conf
 
 systemctl restart frr
+echo "R0 configurado"
 
-echo "Router R0 configurado"
+elif [ "$op" = "2" ]; then
 
-;;
+echo "Configurando R1..."
 
-2.
-
-cat > /etc/frr/frr.conf <<EOF
-hostname R1
+echo "hostname R1
 service integrated-vtysh-config
-
 ipv6 forwarding
-
 interface enp0s3
 ipv6 address 2001:4::1/64
 ipv6 rip RIP enable
-
 interface enp0s8
 ipv6 address 2001:5::1/64
 ipv6 rip RIP enable
-
 interface enp0s9
 ipv6 address 2001:3::2/64
 ipv6 rip RIP enable
-
 ipv6 router rip RIP
-
-line vty
-EOF
+line vty" > /etc/frr/frr.conf
 
 systemctl restart frr
+echo "R1 configurado"
 
-echo "Router R1 configurado"
+else
 
-;;
-
-3.
-
-exit
-;;
-
-*)
 echo "Opcion invalida"
-;;
 
-esac
-
-done
+fi
