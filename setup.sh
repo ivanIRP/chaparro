a57b245ba62ffb87728f228a0098fcf704eabbe5
ivanIRP <ivatar1066@gmail.com>
2026-04-23 16:52:31 -0600

    ajustes

--- setup.sh
+++ setup.sh
@@ -1,245 +1,108 @@
 #!/bin/bash
-# Auto-configurador de Routers Debian 12 (Topologia IPv6 sobre IPv4) FINAL 2.0
 
-# ==========================================
-# 1. DEFINE TUS INTERFACES DE LINUX AQUI
-# ==========================================
-IF_G0="enp0s3"  # Hacia la PC local
-IF_S0="enp0s8"  # Conexion Izquierda
-IF_S1="enp0s9"  # Conexion Derecha
-
-# ==========================================
-# Inicio del Script
-# ==========================================
-if [ "$EUID" -ne 0 ]; then
-  echo "Por favor, ejecuta este script como root (sudo o su -)"
-  exit 1
+# --- Comprobación de privilegios ---
+if [[ $EUID -ne 0 ]]; then
+   echo "Este script debe ejecutarse como root (sudo)." 
+   exit 1
 fi
 
-echo "======================================="
-echo "   CONFIGURACION DE ROUTER DEBIAN 12   "
-echo "======================================="
-read -p "Que router vas a configurar? (Ingresa del 0 al 5): " ROUTER_NUM
+# --- Configuración Global de Forwarding ---
+echo "[*] Habilitando IP Forwarding (v4 y v6)..."
+sysctl -w net.ipv4.ip_forward=1 > /dev/null
+sysctl -w net.ipv6.conf.all.forwarding=1 > /dev/null
 
-# Habilitar IP Forwarding Global (A prueba de fallos)
-echo 1 > /proc/sys/net/ipv4/ip_forward
-echo 1 > /proc/sys/net/ipv6/conf/all/forwarding
-echo 1 > /proc/sys/net/ipv6/conf/default/forwarding
+# --- Menú de Selección ---
+echo "------------------------------------------------"
+echo " CONFIGURACIÓN DE ROUTERS FRR - PROYECTO REDES "
+echo "------------------------------------------------"
+echo "Selecciona el router que deseas configurar:"
+echo "0) R0 (Borde IPv6 -> IPv4)"
+echo "1) R1 (Gateway Dual Stack)"
+echo "2) R2 (Core IPv6)"
+echo "3) R3 (Gateway Dual Stack)"
+echo "4) R4 (Core IPv4)"
+echo "5) R5 (Borde IPv4 -> IPv6)"
+read -p "Opción [0-5]: " OPCION
 
-# Instalar FRR si no esta instalado
-if ! command -v vtysh &> /dev/null; then
-    echo "Instalando FRRouting (FRR)..."
-    apt-get update
-    apt-get install -y frr
-fi
-
-# Habilitar RIP y OSPF6 en FRR
-sed -i 's/ripd=no/ripd=yes/' /etc/frr/daemons
-sed -i 's/ospf6d=no/ospf6d=yes/' /etc/frr/daemons
-
-# Limpiar solo las IPv6 globales y tuneles
-ip -6 addr flush dev $IF_G0 scope global 2>/dev/null
-ip -6 addr flush dev $IF_S0 scope global 2>/dev/null
-ip -6 addr flush dev $IF_S1 scope global 2>/dev/null
-ip -4 addr flush dev $IF_S0 2>/dev/null
-ip -4 addr flush dev $IF_S1 2>/dev/null
-ip tunnel del tun0 2>/dev/null
-
-# Preparar archivo FRR base (Se quito 'no ipv6 forwarding')
-FRR_CONF="/etc/frr/frr.conf"
-cat <<EOF > $FRR_CONF
-frr defaults traditional
-hostname R$ROUTER_NUM
-ipv6 forwarding
-!
-EOF
-
-case $ROUTER_NUM in
-  0)
-    echo "Configurando R0 (Extremo IPv6)..."
-    ip link set up dev $IF_G0
-    ip -6 addr add 2000::1/64 dev $IF_G0
-    
-    ip link set up dev $IF_S0
-    ip -6 addr add 2002::1/64 dev $IF_S0
-    # Forzar Link-Local
-    ip -6 addr add fe80::10/64 dev $IF_S0 scope link
-
-    cat <<EOF >> $FRR_CONF
-interface $IF_G0
- ipv6 ospf6 area 0
-!
-interface $IF_S0
- ipv6 ospf6 network point-to-point
- ipv6 ospf6 area 0
-!
-router ospf6
- ospf6 router-id 1.1.1.1
- redistribute connected
-!
-EOF
-    ;;
-    
-  1)
-    echo "Configurando R1 (Inicio del Tunel)..."
-    ip link set up dev $IF_G0
-    ip -6 addr add 2001::1/64 dev $IF_G0
-    
-    ip link set up dev $IF_S0
-    ip -6 addr add 2002::2/64 dev $IF_S0
-    # Forzar Link-Local
-    ip -6 addr add fe80::11/64 dev $IF_S0 scope link
-    
-    ip link set up dev $IF_S1
-    ip addr add 192.168.0.1/30 dev $IF_S1
-    
-    # Crear Tunel
-    ip tunnel add tun0 mode sit remote 192.168.0.10 local 192.168.0.1 ttl 255
-    ip link set up dev tun0
-    ip -6 addr add 3000::1/64 dev tun0
-    
-    # NUEVO: Forzar Link-Local en el tunel para evitar drop de pings
-    ip -6 addr add fe80::1/64 dev tun0 scope link
-    
-    # Forzar forwarding en el tunel
-    echo 1 > /proc/sys/net/ipv6/conf/tun0/forwarding 2>/dev/null
-
-    cat <<EOF >> $FRR_CONF
-interface $IF_G0
- ipv6 ospf6 area 0
-!
-interface $IF_S0
- ipv6 ospf6 network point-to-point
- ipv6 ospf6 area 0
-!
-interface tun0
- ipv6 ospf6 network point-to-point
- ipv6 ospf6 area 0
-!
-router rip
- version 2
- network 192.168.0.0/24
-!
-router ospf6
- ospf6 router-id 1.1.1.2
- redistribute connected
-!
-EOF
-    ;;
-    
-  2)
-    echo "Configurando R2 (Transito IPv4)..."
-    ip link set up dev $IF_S0
-    ip addr add 192.168.0.2/30 dev $IF_S0
-    
-    ip link set up dev $IF_S1
-    ip addr add 192.168.0.5/30 dev $IF_S1
-
-    cat <<EOF >> $FRR_CONF
-router rip
- version 2
- network 192.168.0.0/24
-!
-EOF
-    ;;
-    
-  3)
-    echo "Configurando R3 (Transito IPv4)..."
-    ip link set up dev $IF_S0
-    ip addr add 192.168.0.6/30 dev $IF_S0
-    
-    ip link set up dev $IF_S1
-    ip addr add 192.168.0.9/30 dev $IF_S1
-
-    cat <<EOF >> $FRR_CONF
-router rip
- version 2
- network 192.168.0.0/24
-!
-EOF
-    ;;
-    
-  4)
-    echo "Configurando R4 (Fin del Tunel)..."
-    ip link set up dev $IF_G0
-    ip -6 addr add 2003::1/64 dev $IF_G0
-    
-    ip link set up dev $IF_S0
-    ip addr add 192.168.0.10/30 dev $IF_S0
-    
-    ip link set up dev $IF_S1
-    ip -6 addr add 2005::1/64 dev $IF_S1
-    # Forzar Link-Local
-    ip -6 addr add fe80::14/64 dev $IF_S1 scope link
-    
-    # Crear Tunel
-    ip tunnel add tun0 mode sit remote 192.168.0.1 local 192.168.0.10 ttl 255
-    ip link set up dev tun0
-    ip -6 addr add 3000::2/64 dev tun0
-    
-    # NUEVO: Forzar Link-Local en el tunel para evitar drop de pings
-    ip -6 addr add fe80::4/64 dev tun0 scope link
-    
-    # Forzar forwarding en el tunel
-    echo 1 > /proc/sys/net/ipv6/conf/tun0/forwarding 2>/dev/null
-
-    cat <<EOF >> $FRR_CONF
-interface $IF_G0
- ipv6 ospf6 area 0
-!
-interface $IF_S1
- ipv6 ospf6 network point-to-point
- ipv6 ospf6 area 0
-!
-interface tun0
- ipv6 ospf6 network point-to-point
- ipv6 ospf6 area 0
-!
-router rip
- version 2
- network 192.168.0.0/24
-!
-router ospf6
- ospf6 router-id 1.1.1.3
- redistribute connected
-!
-EOF
-    ;;
-    
-  5)
-    echo "Configurando R5 (Extremo IPv6)..."
-    ip link set up dev $IF_G0
-    ip -6 addr add 2004::1/64 dev $IF_G0
-    
-    ip link set up dev $IF_S0
-    ip -6 addr add 2005::2/64 dev $IF_S0
-    # Forzar Link-Local
-    ip -6 addr add fe80::15/64 dev $IF_S0 scope link
-
-    cat <<EOF >> $FRR_CONF
-interface $IF_G0
- ipv6 ospf6 area 0
-!
-interface $IF_S0
- ipv6 ospf6 network point-to-point
- ipv6 ospf6 area 0
-!
-router ospf6
- ospf6 router-id 1.1.1.4
- redistribute connected
-!
-EOF
-    ;;
-    
-  *)
-    echo "Error: Numero no valido. Debe ser del 0 al 5."
-    exit 1
-    ;;
+case $OPCION in
+    0)
+        echo "Configurando R0..."
+        # Interfaces
+        ip addr add 2000::2/125 dev eth0 2>/dev/null
+        ip addr add 200.0.0.1/30 dev eth1 2>/dev/null
+        ip link set eth0 up && ip link set eth1 up
+        # Túnel SIT (IPv6 over IPv4) hacia R1
+        ip tunnel add tunR1 mode sit remote 200.0.0.2 local 200.0.0.1
+        ip link set tunR1 up
+        ip addr add 2000::19/125 dev tunR1
+        # Enrutamiento FRR
+        vtysh -c "conf t" -c "ipv6 route 2000::/121 tunR1" -c "exit" -c "write"
+        ;;
+    1)
+        echo "Configurando R1..."
+        ip addr add 200.0.0.2/30 dev eth0 2>/dev/null
+        ip addr add 2000::17/125 dev eth1 2>/dev/null
+        ip addr add 8.0.0.254/24 dev eth2 2>/dev/null
+        ip link set eth0 up && ip link set eth1 up && ip link set eth2 up
+        # Túnel v6 sobre v4 hacia R0
+        ip tunnel add tunR0 mode sit remote 200.0.0.1 local 200.0.0.2
+        ip link set tunR0 up
+        # Túnel GRE (v4 sobre v6) hacia R3
+        ip link add tunR3 type ip6gre local 2000::17 remote 2000::22
+        ip link set tunR3 up
+        ip addr add 10.255.0.1/30 dev tunR3
+        # Enrutamiento
+        vtysh -c "conf t" -c "ipv6 route 2000::/125 tunR0" -c "ip route 9.0.0.0/24 tunR3" -c "ip route 11.0.0.0/24 tunR3" -c "exit" -c "write"
+        ;;
+    2)
+        echo "Configurando R2..."
+        ip addr add 2000::18/125 dev eth0 2>/dev/null
+        ip addr add 2000::21/125 dev eth1 2>/dev/null
+        ip addr add 2000::14/125 dev eth2 2>/dev/null
+        ip link set eth0 up && ip link set eth1 up && ip link set eth2 up
+        # Solo rutas estáticas IPv6
+        vtysh -c "conf t" -c "ipv6 route 2000::/125 2000::17" -c "exit" -c "write"
+        ;;
+    3)
+        echo "Configurando R3..."
+        ip addr add 2000::22/125 dev eth0 2>/dev/null
+        ip addr add 200.0.0.5/30 dev eth1 2>/dev/null
+        ip addr add 9.0.0.254/24 dev eth2 2>/dev/null
+        ip link set eth0 up && ip link set eth1 up && ip link set eth2 up
+        # Túnel v4 sobre v6 hacia R1
+        ip link add tunR1 type ip6gre local 2000::22 remote 2000::17
+        ip link set tunR1 up
+        ip addr add 10.255.0.2/30 dev tunR1
+        # Túnel v6 sobre v4 hacia R4
+        ip tunnel add tunR4 mode sit remote 200.0.0.6 local 200.0.0.5
+        ip link set tunR4 up
+        # Enrutamiento
+        vtysh -c "conf t" -c "ip route 8.0.0.0/24 tunR1" -c "ipv6 route 2000::10/125 tunR4" -c "exit" -c "write"
+        ;;
+    4)
+        echo "Configurando R4..."
+        ip addr add 200.0.0.6/30 dev eth0 2>/dev/null
+        ip addr add 200.0.0.9/30 dev eth1 2>/dev/null
+        ip addr add 2000::13/125 dev eth2 2>/dev/null
+        ip link set eth0 up && ip link set eth1 up && ip link set eth2 up
+        # El Core v4 encamina tráfico IPv4 de los túneles de otros routers
+        vtysh -c "conf t" -c "ip route 8.0.0.0/24 200.0.0.5" -c "ip route 11.0.0.0/24 200.0.0.10" -c "exit" -c "write"
+        ;;
+    5)
+        echo "Configurando R5..."
+        ip addr add 200.0.0.10/30 dev eth0 2>/dev/null
+        ip addr add 11.0.0.254/24 dev eth1 2>/dev/null
+        ip link set eth0 up && ip link set eth1 up
+        # Túnel hacia R3 (cruzando el core v4)
+        ip tunnel add tunR3 mode sit remote 200.0.0.5 local 200.0.0.10
+        ip link set tunR3 up
+        # Enrutamiento
+        vtysh -c "conf t" -c "ip route 8.0.0.0/24 200.0.0.9" -c "ip route 9.0.0.0/24 200.0.0.9" -c "exit" -c "write"
+        ;;
+    *)
+        echo "Opción no válida."
+        exit 1
+        ;;
 esac
 
-# Reiniciar FRR
-systemctl restart frr
-
-echo "======================================="
-echo "Configuracion de R$ROUTER_NUM completada y optimizada."
-echo "======================================="
+echo "[!] Configuración aplicada con éxito para el router seleccionado."
